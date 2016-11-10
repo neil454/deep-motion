b16ba780e49442d3018ce44ad3719ee6e04d9f4f
Neil <neil454@gmail.com>
2016-11-10 01:08:39 -0500

    added adapted script to scrap video id's from youtube-8m added preliminary script to batch download a list of video files, and only download vids that follow the filter, currently only 720p60 videos

---
+++ src/batch_video_download.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+import os
+import youtube_dl
+# (https://github.com/rg3/youtube-dl/)
+
+vid_list = [
+    'GCx5oPWRKbc',  # no 1280x720 res, should error
+    'sC9abcLLQpI',  # no 60fps, should error
+    'rql_F8H3h9E',
+]
+
+# more info about options here:
+# https://github.com/rg3/youtube-dl/blob/master/youtube_dl/YoutubeDL.py#L128-L278
+ydl_opts = {
+    # 'listformats': True,
+    'ignoreerrors': True,
+    # TODO use only 720p60fps video or not?
+    'format': 'bestvideo[ext=mp4][width = 1280][height = 720][fps = 60]',
+    # For debugging...
+    # 'skip_download': True
+}
+
+# save all downloaded videos to proper folder
+os.chdir("../data/youtube-8m-videos")
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(vid_list)
---
+++ src/scrape_youtube_8m_ids.py
@@ -0,0 +1,41 @@
+# adapted from http://codegists.com/snippet/python/youtube-8m-video-id-scraperpy_josephredfern_python
+
+import os
+import requests
+from collections import defaultdict
+
+os.chdir("../data/youtube-8m-id-scrape")
+
+csv_prefix = "http://www.yt8m.org/csv"
+
+r = requests.get("{0}/verticals.json".format(csv_prefix))
+verticals = r.json()
+
+block_urls = defaultdict(list)
+count = 0
+for cat, urls in verticals.items():
+    for url in urls:
+        jsurl = "{0}/j/{1}.js".format(csv_prefix, url.split("/")[-1])
+        block_urls[cat[1:]].append(jsurl)
+        count += 1  # lazy.
+
+ids_by_cat = defaultdict(list)
+
+downloaded = 0.0
+for cat_name, block_file_urls in block_urls.items():
+    for block_file_url in block_file_urls:
+        print("[{0}%] Downloading block file: {1} {2}".format((100.0 * downloaded / count), block_file_url, cat_name))
+        try:
+            r = requests.get(block_file_url)
+            idlist = r.content.split("\"")[3]
+            ids = [n for n in idlist.split(";") if len(n) > 3]
+            ids_by_cat[cat_name] += ids
+        except IndexError, IOError:
+            print("Failed to download or process block at {0}".format(block_file_url))
+        downloaded += 1  # increment even if we've failed.
+
+    with open("{0}.txt".format(cat_name), "w") as idfile:
+        print("Writing ids to {0}.txt".format(cat_name))
+        for vid in ids_by_cat[cat_name]:
+            idfile.write("{0}\n".format(vid))
+        print("Done.")
